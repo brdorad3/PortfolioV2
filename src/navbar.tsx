d09a965172130d074b878c65c0099deb0a226939
adis <vunicadis@gmail.com>
2024-06-21 10:15:12 +0200

    f

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -4,17 +4,17 @@ import { HashLink } from 'react-router-hash-link';
 const Navbar = () => {
     return(<div className="flex w-full bg-black justify-between px-24 py-5 border-b-2 border-cream fixed max-sm:px-5 z-10 ">   
         <div className="flex flex-col">
-        <h1 className="text-white text-md ">Brdorad3</h1>
+        <h1 className="text-white text-base chakra ">Brdorad3</h1>
         <div className=" w-1/2 h-[1px] bg-white"></div>
         </div>
         <section className="flex gap-5">
             
         
-        <HashLink smooth to='#stack' > <h2 className="text-white text-base">Stack</h2> </HashLink>
+        <HashLink smooth to='#stack' > <h2 className="text-white text-base chakra">Stack</h2> </HashLink>
         <h3 className='text-white cursor-pointer '>|</h3>
-        <HashLink smooth to='#projects' > <h2 className="text-white  text-base">Projects</h2> </HashLink>
+        <HashLink smooth to='#projects' > <h2 className="text-white  text-base chakra">Projects</h2> </HashLink>
         <h3 className='text-white  cursor-pointer'>|</h3>
-        <HashLink smooth to='#contact' > <h2 className="text-white text-base">Contact</h2> </HashLink>
+        <HashLink smooth to='#contact' > <h2 className="text-white text-base chakra">Contact</h2> </HashLink>
         
         </section>
 
